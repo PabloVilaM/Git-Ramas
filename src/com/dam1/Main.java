191e8f3480592f3587ad98df825a4e6ad885909c
Pablo Vila Mauricio <pvilamauricio@danielcastelao.org>
2022-01-26 13:34:50 +0100

    Haciendo prueba d

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -3,6 +3,6 @@ package com.dam1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola esto es la tercera version (pruebas)");
+        System.out.println("Hola esto es la cuarta version (pruebas)");
     }
 }
